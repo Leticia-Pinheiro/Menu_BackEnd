43637ed9f8b03dff60bffd26c84df20209259e4d
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-11-09 20:46:00 -0300

    controller

---
+++ src/controllers/revenueController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express"
+import * as revenueService from "../services/revenueService"
+
+export async function createRevenue(req: Request, res: Response){
+    const revenue = req.body
+
+    await revenueService.createRevenue(revenue)
+
+    res.send("Revenue created successfully").status(201)
+}
+
+export async function getRevenue(req: Request, res: Response){
+    const revenuesData = await revenueService.getRevenues()
+
+    res.send(revenuesData).status(201)
+}
